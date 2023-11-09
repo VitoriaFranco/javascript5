f5f761d7968c3c89c8afb181d4c9269c763a63aa
VitoriaFranco <128157948+VitoriaFranco@users.noreply.github.com>
2023-11-09 08:51:31 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
+function potencia(){
+let n = parseInt (numero.value);
+let pote = n;
+if(n<0){
+alerta("numero negativo");    
+}
+else if(n==0){
+    alert("1")
+}
+else{
+while (n>=1){
+pote = Math.pow(2,n);
+n = n-1
+alert(pote);
+   }
+ }
+}
+
 function soma_seg(){
-
-var n = 0
-while (n < 5)
-n = n + 1
-soma + soma + n + 1
-var n = parseInt(numero.valiue);
-var soma = n;
-
-
-
-
-
+    var n = parseInt(numero.value);
+    var soma = n;
+    with(n>1){
+soma = soma + (n-1);
+n = n-1;
+    }
+    alert(soma);
 }
